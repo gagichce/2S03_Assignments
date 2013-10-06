fb0d79fc6bb417166fcbf4046bb80d9767a6b141
gagichce <gagichce@mcmaster.ca>
2013-10-06 15:17:59 -0400

    The actual completed assignment

---
+++ Assignment 1/Completed Assignment/se2s03/A1.java
@@ -0,0 +1,24 @@
+package se2s03;
+
+public class A1 {
+    public int cases(int v, int u, int w) {
+    	if (v <=3){
+    		if (u <=-5){
+    			if(w <= 12)
+        			return 7;
+        		return 9;
+        	}
+    		if (w <=4)
+    			return 10;
+    		return 1;
+    	}
+    	if ( w <=2){
+    		if (u<=9)
+    			return 6;
+    		return 3;	
+		}
+    	if(u <=-6)
+			return 5;
+		return 2;
+    } 
+}
