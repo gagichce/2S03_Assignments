a8f9bce05416bf1febcb02b50897b92bc230eb0e
gagichce <gagichce@mcmaster.ca>
2013-10-19 17:51:54 -0400

    added some parallels, it is faster now

--- Assignment 2/Assignment 2 Generator/Assignment 2 Generator/Program.cs
+++ Assignment 2/Assignment 2 Generator/Assignment 2 Generator/Program.cs
@@ -15,7 +15,16 @@ namespace Assignment_2_Generator
             List<int> studentNumbers = thisParser.getStudentNumbers();
             thisParser.changeAddress("http://www.cas.mcmaster.ca/~se2s03/assignments/a2/");
             ReadWrite writer = new ReadWrite();
-            foreach (int studentNumber in studentNumbers)
+            //foreach (int studentNumber in studentNumbers)
+            //{
+            //    StudentSolution thisSolution = new StudentSolution(studentNumber, thisParser.getBadCode(studentNumber));
+            //    thisSolution.step1 = Stepper.BadToOne(thisSolution.BadCode);
+            //    thisSolution.step2 = Stepper.OneToTwo(thisSolution.step1);
+            //    thisSolution.step3 = Stepper.TwoToThree(thisSolution.step2);
+            //    thisSolution.step4 = Stepper.ThreeToFour(thisSolution.step3);
+            //    writer.WriteStudentSolution(thisSolution);
+            //}
+            Parallel.ForEach(studentNumbers, studentNumber =>
             {
                 StudentSolution thisSolution = new StudentSolution(studentNumber, thisParser.getBadCode(studentNumber));
                 thisSolution.step1 = Stepper.BadToOne(thisSolution.BadCode);
@@ -23,7 +32,7 @@ namespace Assignment_2_Generator
                 thisSolution.step3 = Stepper.TwoToThree(thisSolution.step2);
                 thisSolution.step4 = Stepper.ThreeToFour(thisSolution.step3);
                 writer.WriteStudentSolution(thisSolution);
-            }
+            });
         }
     }
 }
